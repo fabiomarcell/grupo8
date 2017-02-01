28b46b94352877265353712e5aef5c9be2f5fe64
Fabio Marcell <fabio.marcell@outlook.com>
2017-01-31 22:48:09 -0200

    layout paginação

--- actions.php
+++ actions.php
@@ -11,7 +11,21 @@
         $pg += 1;
         $html = "";
         foreach ( $registros as $registro ) {
-            $html .= '';
+            $html .= '<div class="col-md-4 item-block animate-box" data-animate-effect="fadeIn">
+                    <div class="fh5co-property">
+                        <figure>
+                            <img src="grupo8/upload/img/'.$registro['foto'].'" alt="" class="img-responsive">
+                            <a href="#" class="tag">'.$registro['cupomTitulo'].'</a>
+                        </figure>
+                        <div class="fh5co-property-innter">
+                            <div class="price-status">
+                            <span class="price">R$ '. number_format($registro['cupomValorExibir'], 2, ',', '.').' </span>
+                       </div>
+                       <p>'.nl2br($registro['cupomDescricao']).'</p>
+                    </div>
+                    
+                    </div>              
+                </div>';
         }
         die( json_encode( array( "results" => $html, "pg" => $pg, "totalItens" => count( $registros ) ) ) );
     }
