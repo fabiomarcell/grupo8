9fc46a7d29af71d461e4e720d4404398e13e36be
Fabio Marcell <fabio.marcell@outlook.com>
2017-02-01 14:35:31 -0200

    Fix pagination json result

--- actions.php
+++ actions.php
@@ -11,22 +11,13 @@
         $pg += 1;
         $html = "";
         foreach ( $registros as $registro ) {
-            $html .= '<div class="col-md-4">
-                    <div class="fh5co-property" style="margin-bottom: 50px;">
-                        <figure>
-                            <img src="grupo8/'.$registro['foto'].'" alt="" class="img-responsive">
-                            <a href="javascript:void(0);" onclick="checkSession()" class="tag">'.$registro['cupomOrigem'].'</a>
-                        </figure>
-                        <div class="fh5co-property-innter">
-                            <h3>'.$registro['cupomTitulo'].'</h3>
-                            <div class="price-status">
-                            <span class="price">R$ '. number_format($registro['cupomValorExibir'], 2, ',', '.').' </span>
-                       </div>
-                       <p>'.nl2br($registro['cupomDescricao']).'</p>
-                    </div>
-                    
-                    </div>              
-                </div>';
+            $html .= '<div class="cupom">
+                        <img src="grupo8/'.$registro['foto'].'"/>
+                        <h4>'.$registro['cupomTitulo'].'</h4>
+                        <p>'.nl2br($registro['cupomDescricao']).'</p>
+                        <p class="preco">R$ '. number_format($registro['cupomValorExibir'], 2, ',', '.').'</p>
+                        <a href="javascript:void(0);" onclick="checkSession()" class="tag">'.$registro['cupomOrigem'].'</a>
+                      </div>';
         }
         die( json_encode( array( "results" => $html, "pg" => $pg, "totalItens" => count( $registros ) ) ) );
     }
