f01b11566aecd7477dc808af40a6db7e4ba1255f
Fabio Marcell <fabio.marcell@outlook.com>
2017-02-01 00:48:14 -0200

    create php files

---
+++ index.php
@@ -0,0 +1,329 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Bit Gift</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+	<link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+	<!-- <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'> -->
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+	
+	
+	<div id="fh5co-page">
+	<header id="fh5co-header" role="banner">
+		<div class="container">
+			<div class="row">
+				<div class="header-inner">
+					<h1><a href="index.html"><img src="images/Logo.png" style="width:100px"></a></h1>
+					<nav role="navigation">
+						<ul>
+							<li><a href="#sobre">Sobre</a></li>
+							<li><a href="#comoFunciona">Como Funciona?</a></li>
+							<li class="cta"><a href="#contato">Entre em contato</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
+	
+
+	<aside id="fh5co-hero" clsas="js-fullheight">
+		<div class="flexslider js-fullheight">
+			<ul class="slides">
+		   	<li style="background-image: url(images/slide_1.jpg);">
+		   		<div class="container">
+		   			<div class="col-md-12 text-center js-fullheight fh5co-property-brief slider-text">
+		   				<div class="fh5co-property-brief-inner">
+		   					<div class="fh5co-box">
+		   						
+		   						<h3><a href="#">Villa In Hialeah, Dade County</a></h3>
+		   						<div class="price-status">
+                             	<span class="price">$540,000 <a href="#" class="tag">For Sale</a></span>
+	                        </div>
+	                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque dicta magni amet atque doloremque velit unde adipisci omnis hic quaerat.</p>
+
+	                        <p class="fh5co-property-specification">
+                        		<span><strong>3500</strong> Sq Ft</span>  <span><strong>3</strong> Beds</span>  <span><strong>3.5</strong> Baths</span>  <span><strong>2</strong> Garages</span>
+                        	</p>
+
+	                        <p><a href="#" class="btn btn-primary">Learn more</a></p>
+									
+	                        
+	   						</div>
+		   				</div>
+		   			</div>
+		   		</div>
+		   	</li>
+		   	<li style="background-image: url(images/slide_2.jpg);">
+		   		<div class="container">
+		   			<div class="col-md-12 text-center js-fullheight fh5co-property-brief slider-text">
+		   				<div class="fh5co-property-brief-inner">
+		   					<div class="fh5co-box">
+		   						<h3><a href="#">15 Apartments Of Type B</a></h3>
+		   						<div class="price-status">
+                             	<span class="price">$2,200<span class="per">/Month</span> <a href="#" class="tag">For Rent</a></span>
+	                        </div>
+	                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque dicta magni amet atque doloremque velit unde adipisci omnis hic quaerat.</p>
+	                        <p class="fh5co-property-specification">
+                        		<span><strong>3500</strong> Sq Ft</span>  <span><strong>3</strong> Beds</span>  <span><strong>3.5</strong> Baths</span>  <span><strong>2</strong> Garages</span>
+                        	</p>
+	                        <p><a href="#" class="btn btn-primary">Learn more</a></p>
+	   						</div>
+		   				</div>
+		   			</div>
+		   		</div>
+		   	</li>
+				<li style="background-image: url(images/slide_3.jpg);">
+		   		<div class="container">
+		   			<div class="col-md-12 text-center js-fullheight fh5co-property-brief slider-text">
+		   				<div class="fh5co-property-brief-inner">
+		   					<div class="fh5co-box">
+		   						<h3><a href="#">401 Biscayne Boulevard, Miami</a></h3>
+		   						<div class="price-status">
+                             	<span class="price">$1,540,000 <a href="#" class="tag">For Sale</a></span>
+	                        </div>
+	                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque dicta magni amet atque doloremque velit unde adipisci omnis hic quaerat.</p>
+	                        <p class="fh5co-property-specification">
+                        		<span><strong>3500</strong> Sq Ft</span>  <span><strong>3</strong> Beds</span>  <span><strong>3.5</strong> Baths</span>  <span><strong>2</strong> Garages</span>
+                        	</p>
+	                        <p><a href="#" class="btn btn-primary">Learn more</a></p>
+
+
+	   						</div>
+		   				</div>
+		   			</div>
+		   		</div>
+		   	</li>
+		   	
+		  	</ul>
+	  	</div>
+	</aside>
+	<div id="best-deal">
+		<div class="container">
+			<div class="row" id="res">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box" data-animate-effect="fadeIn">
+					<h2>We are Offering the Best Real Estate Deals</h2>
+					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+				</div>
+
+
+				<!--Stuff-->
+				
+
+
+			</div>
+			<p style="width:100%; text-align:center;"><a href="javascript:void(0);" id="nextPage" class="btn btn-primary btn-outline with-arrow">Get started <i class="icon-arrow-right"></i></a></p>
+		</div>
+	</div>
+
+
+
+	<div class="fh5co-section-with-image">
+		
+		<img src="images/image_1.jpg" alt="" class="img-responsive">
+		<div class="fh5co-box animate-box" style="left:10%;">
+			<h2>Sobre</h2>
+			<p> A Bitgift surgiu com o propósito de facilitar suas compras em e-commerces usando bitcoins. Você pode adquirir um vale presente de e-commerces selecionados em nosso site, pagar em bitcoins e usá-lo normalmente na loja virtual. Sem necessidade de efetuar transação com sua corretora, e pagando menos por isso.
+				<br><br>
+Nossa proposta é tornar possíveis suas compras em lojas virtuais que ainda não aceitam bitcoins como forma de pagamento. Com praticidade e agilidade.</p>
+			<p><a href="#" class="btn btn-primary btn-outline with-arrow">Get started <i class="icon-arrow-right"></i></a></p>
+		</div>
+		
+	</div>
+	
+	<div class="fh5co-section-with-image">
+		
+		<img src="images/image_1.jpg" alt="" class="img-responsive">
+		<div class="fh5co-box animate-box">
+			<h2>Como Funciona</h2>
+			<ul>
+				<li>Escolha em nosso site o vale presente de um dos e-commerces parceiros </li>
+				<li>Selecione o valor ($20, $50 ou $100)</li>
+				<li>Efetue o pagamento em bitcoins</li>
+				<li>Receba seu vale presente virtual por e-mail</li>
+				<li>Use nossa solução e boas compras!</li>
+			</ul>
+			<p>
+				E como a Bitgift ganha com isso?
+				<br>
+				Nós cobramos uma taxa mínima pela operação, de acordo com o valor do vale presente selecionado:
+			</p>
+			<ul>
+				<li>15% para o vale presente de $20 </li>
+				<li>10% para o vale presente de $50</li>
+				<li>5% para o vale presente de $100</li>
+			</ul>
+			<p>
+				Nós também recebemos uma comissão do site onde você efetuar sua compra, caso você use o link enviado para seu e-mail para acessar o site parceiro. E você não paga nada a mais por isso.
+			</p>
+			<p><a href="#" class="btn btn-primary btn-outline with-arrow">Get started <i class="icon-arrow-right"></i></a></p>
+		</div>
+		
+	</div>
+
+
+	
+
+	<div class="fh5co-cta" style="background-image: url(images/slide_4.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="col-md-12 text-center animate-box" data-animate-effect="fadeIn">
+				<h3>We Try To Update The Site Everyday</h3>
+				<p><a href="#" class="btn btn-primary btn-outline with-arrow">Get started now! <i class="icon-arrow-right"></i></a></p>
+			</div>
+		</div>
+	</div>
+
+	
+	<footer id="fh5co-footer" role="contentinfo">
+	
+		<div class="container">
+			<div class="col-md-3 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>About Us</h3>
+				<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+				<p><a href="#" class="btn btn-primary btn-outline with-arrow btn-sm">I'm button <i class="icon-arrow-right"></i></a></p>
+			</div>
+			<div class="col-md-6 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Our Services</h3>
+				<ul class="float">
+					<li><a href="#">Web Design</a></li>
+					<li><a href="#">Branding &amp; Identity</a></li>
+					<li><a href="#">Free HTML5</a></li>
+					<li><a href="#">HandCrafted Templates</a></li>
+				</ul>
+				<ul class="float">
+					<li><a href="#">Free Bootstrap Template</a></li>
+					<li><a href="#">Free HTML5 Template</a></li>
+					<li><a href="#">Free HTML5 &amp; CSS3 Template</a></li>
+					<li><a href="#">HandCrafted Templates</a></li>
+				</ul>
+
+			</div>
+
+			<div class="col-md-2 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Follow Us</h3>
+				<ul class="fh5co-social">
+					<li><a href="#"><i class="icon-twitter"></i></a></li>
+					<li><a href="#"><i class="icon-facebook"></i></a></li>
+					<li><a href="#"><i class="icon-google-plus"></i></a></li>
+					<li><a href="#"><i class="icon-instagram"></i></a></li>
+				</ul>
+			</div>
+			
+			
+			<div class="col-md-12 fh5co-copyright text-center">
+				<p>&copy; 2016 Free HTML5 template. All Rights Reserved. <span>Designed with <i class="icon-heart"></i> by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images by <a href="http://unsplash.com/" target="_blank">Unsplash</a></span></p>	
+			</div>
+			
+		</div>
+	</footer>
+	</div>
+	
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+
+	<!-- MAIN JS -->
+	<script src="js/main.js"></script>
+	
+	<span id="page" style="display:none">1</span>
+    <span id="trigger" style="display:none">true</span>
+	<script>
+            $(document).ready(function () {
+                $("#nextPage").click(function (event) {
+                        getPagination($("#page").html());
+                });
+            });
+            function getPagination(pg) {
+                if ($("#trigger").html() === "true") {
+                    $("#trigger").html("false");
+                    $("#res").fadeTo("slow", 0.3);
+                    setTimeout(function () {
+                        $.ajax({
+                            type: "POST",
+                            url: "actions.php",
+                            data: {
+                                exec: 'scroll',
+                                pg: pg
+                            },
+                            dataType: 'json',
+                            processData: true,
+                            success: function (data) {
+                                if (data.results !== "") {
+                                	console.log(data);
+                                    $("#res").append(data.results);
+                                    setTimeout(function () {
+                                        if (data.totalItens < 6) {
+                                            $("#trigger").html("false");
+                                            $("#nextPage").fadeOut();
+                                        }
+                                        else {
+                                            $("#trigger").html("true");
+                                        }
+                                        $("#page").html(data.pg);
+                                    }, 500);
+                                }
+                                else {
+                                    $("#res").append('<div class="col-md-12"><h3>Sem Mais Resultados...</h3></div>');
+                                }
+                                $("#res").fadeTo("slow", 1);
+                            }
+                        });
+                    }, 1000);
+                }
+            }
+            getPagination(1);
+        </script>
+	
+	</body>
+</html>
+
