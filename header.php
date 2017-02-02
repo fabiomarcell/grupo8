65a1d4969d1dbd54952aee24d224a1381d359467
Fabio Marcell <fabio.marcell@outlook.com>
2017-02-02 00:12:43 -0200

    Layout info

--- header.php
+++ header.php
@@ -67,6 +67,16 @@
 	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5H74KP4"
 	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	<!-- End Google Tag Manager (noscript) -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-91332727-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
   <header>
     <div class="centro">
       <a href="/">
