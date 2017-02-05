abb6c88763a99524124645c8d1df99b15e0338a5
Fabio Marcell <fabio.marcell@outlook.com>
2017-02-05 11:48:12 -0200

    Layout stuff

--- index.php
+++ index.php
@@ -103,7 +103,7 @@
 		<section style="background-color:#333333; text-align:center; color:#FFF;" >
 	    	BitGift - Aqui seus Bitcoins viram presentes © - 2017 | contato@bitgift.com.br - (11) 983054953
 	    	<section style="text-align:center;">
-	    		<a href="https://www.facebook.com/BitGift-177380176081273/?" target="_blank" style="margin-left:25px; color:#FFF;"><strong>facebook</sontrg></a>
+	    		<a href="https://www.facebook.com/BitGift-177380176081273/?" target="_blank" style="margin-left:25px; color:#FFF;"><img src="img/Logo_fbBitGift.png"></a>
 	    	</section>
     	</section>
     	
